dc8485c59ec72c8045a20b088a1d4f00d4e48597
santiagoalfarorivera <santiago.alfaro@ucsp.edu.pe>
2026-05-22 10:01:28 -0500

    fix: Update Santiago
    Update:
    centro_salud

--- 03_Updates_Tablas.sql
+++ 03_Updates_Tablas.sql
@@ -6,3 +6,7 @@ WHERE id_consulta = 2;
 UPDATE triaje
 SET presi_art_si = 120, presi_art_di = 80
 WHERE id_consulta = 6;
+
+UPDATE centro_salud
+SET nivel_atencion = 'III'
+WHERE nombre = 'Clínica San Pablo';
