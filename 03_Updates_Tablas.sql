9da2de875a4c24fcbe8d1f690c0d2e3514d2405c
ricardohuancap <edwin.huanca@ucsp.edu.pe>
2026-05-25 21:55:30 -0500

    feat: updates en tablas Ricardo
    update en la tabla historia clinica

--- 03_Updates_Tablas.sql
+++ 03_Updates_Tablas.sql
@@ -69,3 +69,8 @@ WHERE nomb_rol = 'médico';
 UPDATE pacientes
 SET direccion='Jiron las Condes 6767', telefono='999967769'
 WHERE apellidos='Vargas Quispe';
+
+
+Update historia clinica
+set obser_general ='faringitis aguda bacterian en 2026'
+where id_paciente=8;
