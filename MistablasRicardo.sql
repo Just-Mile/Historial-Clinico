7a5eec71ebf22717facb728066f86a1fcc1e99db
ricardohuancap <edwin.huanca@ucsp.edu.pe>
2026-06-04 18:58:02 -0500

    deshacer 4to cambio

--- MistablasRicardo.sql
+++ MistablasRicardo.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS pacientes (
     id_paciente   SERIAL PRIMARY KEY,
-    dni           VARCHAR(8)  NOT NULL,
-    nombres       VARCHAR(50) NOT NULL,
+    id_ubigeo     INT,
+    dni           VARCHAR(20)  NOT NULL,
+    nombres       VARCHAR(100) NOT NULL,
     apellidos     VARCHAR(100) NOT NULL,
     fecha_nac     DATE         NOT NULL,
     sexo          CHAR(1)      NOT NULL,
@@ -13,7 +14,7 @@ CREATE TABLE IF NOT EXISTS pacientes (
 
 CREATE TABLE IF NOT EXISTS centro_salud (
     id_centro     SERIAL PRIMARY KEY,
-    nombre        VARCHAR(50) NOT NULL,
+    nombre        VARCHAR(200) NOT NULL,
     direccion     VARCHAR(200) NOT NULL,
     nivel_atencion VARCHAR(50) NOT NULL,
     distrito      VARCHAR(100)
@@ -21,11 +22,11 @@ CREATE TABLE IF NOT EXISTS centro_salud (
 
 CREATE TABLE IF NOT EXISTS historia_clinica (
     id_historia    SERIAL PRIMARY KEY,
-    id_centro      INT          NOT NULL,
+    
     id_paciente    INT          NOT NULL,
     num_hc         VARCHAR(50)  NOT NULL,
     fech_apertura  DATE         NOT NULL,
-    estado         VARCHAR(20),
+    estado         VARCHAR(20)  NOT NULL,
     obser_general  VARCHAR(500)
 );
 
@@ -33,13 +34,14 @@ CREATE TABLE IF NOT EXISTS consulta (
     id_consulta    SERIAL PRIMARY KEY,
     id_historia    INT          NOT NULL,
     id_personal    INT          NOT NULL,
-    id_triaje      INT          NOT NULL,
+    
     
     fecha          DATE         NOT NULL,
     hora           TIME         NOT NULL,
     tipo           VARCHAR(50)  NOT NULL,
     motivo         VARCHAR(500) NOT NULL,
     estado_paci    VARCHAR(50)  NOT NULL,
+    pre_registro_triage VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS examen_aux (
@@ -50,7 +52,7 @@ CREATE TABLE IF NOT EXISTS examen_aux (
     tipo            VARCHAR(50)  NOT NULL,
     nombre          VARCHAR(200) NOT NULL,
     fech_soli       DATE         NOT NULL,
-    estado          VARCHAR(20)
+    estado          VARCHAR(20)  NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS prescripcion (
@@ -60,5 +62,5 @@ CREATE TABLE IF NOT EXISTS prescripcion (
     dosis           VARCHAR(100) NOT NULL,
     frecuencia      VARCHAR(100) NOT NULL,
     duracion_dias   INT          NOT NULL,
-    via_admin       VARCHAR(50)
+    via_admin       VARCHAR(50)  NOT NULL
 );
