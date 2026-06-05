36e0dce6f0330632039995d9006ffab8a4ed0b80
ricardohuancap <edwin.huanca@ucsp.edu.pe>
2026-06-04 19:21:37 -0500

    deshacer 5to cambio

--- Mis_tablas_Santiago.sql
+++ Mis_tablas_Santiago.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS triaje (
      id_triaje      SERIAL PRIMARY KEY,
-    
+    id_consulta    INT          NOT NULL,
     peso_kg        NUMERIC(5,2) NOT NULL,
     talla_cm       NUMERIC(5,1) NOT NULL,
-    fech_hora_toma TIMESTAMP,
+    fech_hora_toma TIMESTAMP    NOT NULL,
     presi_art_si   SMALLINT,
     presi_art_di   SMALLINT,
     rpm            SMALLINT,
@@ -25,11 +25,12 @@ CREATE TABLE IF NOT EXISTS func_bio (
 
 CREATE TABLE IF NOT EXISTS antecedente (
     id_antecedente  SERIAL PRIMARY KEY,
-    id_historia     INT          NOT NULL,
+     id_antecedente2 INT,
+    id_consulta     INT          NOT NULL,
     tipo            VARCHAR(50)  NOT NULL,
     fech_registro   DATE         NOT NULL,
-    gravedad        VARCHAR(30),
-    descripcion     VARCHAR(500)
+    gravedad        VARCHAR(30)  NOT NULL,
+    descripcion     VARCHAR(500) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS diagnostico (
@@ -42,17 +43,17 @@ CREATE TABLE IF NOT EXISTS diagnostico (
 );
 
 CREATE TABLE IF NOT EXISTS enfermedad_actual (
-    id_enfermedad_act   SERIAL PRIMARY KEY,
+    id_enfermedad   SERIAL PRIMARY KEY,
     id_consulta     INT          NOT NULL,
     fecha_ini       DATE         NOT NULL,
-    tiempo_enf      VARCHAR(100),
+    tiempo_enf      VARCHAR(100) NOT NULL,
     descripcion     VARCHAR(500)
 );
 
 CREATE TABLE IF NOT EXISTS tratamiento (
     id_tratamiento  SERIAL PRIMARY KEY,
     id_consulta     INT          NOT NULL,
-    id_personal     INT          NOT NULL,
+    id_personal     INT          ,
     tipo            VARCHAR(50)  NOT NULL,
     fech_ini        DATE         NOT NULL,
     fecha_fin       DATE,
