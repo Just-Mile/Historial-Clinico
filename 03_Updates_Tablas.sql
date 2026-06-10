f60d53763f6aff0e19168055fe4e4f10d0bc7258
ricardohuancap <edwin.huanca@ucsp.edu.pe>
2026-06-10 01:38:54 -0500

    feat: corregir
    por las nuevas tablas

--- 03_Updates_Tablas.sql
+++ 03_Updates_Tablas.sql
@@ -1,10 +1,10 @@
 UPDATE triaje
 SET peso_kg = 75.0, temp = 37.5
-WHERE id_consulta = 2;
+WHERE id_triaje = 2;
 
 UPDATE triaje
 SET presi_art_si = 120, presi_art_di = 80
-WHERE id_consulta = 6;
+WHERE id_triaje = 6;
 
 UPDATE centro_salud
 SET nivel_atencion = 'III'
