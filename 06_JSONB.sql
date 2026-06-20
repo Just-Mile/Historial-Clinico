9761b2d1955a8c96b035928edba417cca06f7df9
santiagoalfarorivera <santiago.alfaro@ucsp.edu.pe>
2026-06-20 16:38:10 -0500

    feat: Create 06_JSONB.sql

---
+++ 06_JSONB.sql
@@ -0,0 +1,30 @@
+ALTER TABLE result_exam ADD COLUMN IF NOT EXISTS resultado_detalle JSONB;
+	-- Laboratorio
+UPDATE result_exam r
+SET resultado_detalle = '{"hemoglobina": "14.2 g/dL", "leucocitos": "7500/mm3", "plaquetas": "250000/mm3", "glucosa": "95 mg/dL"}'
+FROM examen_aux e
+WHERE r.id_examen = e.id_examen AND e.tipo = 'laboratorio';
+
+-- Imagen
+UPDATE result_exam r
+SET resultado_detalle = '{"hallazgo": "Sin consolidaciones", "indice_cardiotoracico": "0.45", "tecnica": "PA y lateral"}'
+FROM examen_aux e
+WHERE r.id_examen = e.id_examen AND e.tipo = 'imagen';
+
+-- Anatomía patológica
+UPDATE result_exam r
+SET resultado_detalle = '{"tipo_tejido": "epitelial", "celulas_atipicas": false, "diagnostico_histologico": "tejido benigno"}'
+FROM examen_aux e
+WHERE r.id_examen = e.id_examen AND e.tipo = 'anatomía patológica';
+
+-- Microbiología
+UPDATE result_exam r
+SET resultado_detalle = '{"microorganismo": "Escherichia coli", "sensibilidad": ["ampicilina", "ciprofloxacino"], "ufc": "100000/mL"}'
+FROM examen_aux e
+WHERE r.id_examen = e.id_examen AND e.tipo = 'microbiología';
+
+-- Genético
+UPDATE result_exam r
+SET resultado_detalle = '{"gen_analizado": "BRCA1", "mutacion_detectada": false, "metodo": "secuenciación"}'
+FROM examen_aux e
+WHERE r.id_examen = e.id_examen AND e.tipo = 'genético';
